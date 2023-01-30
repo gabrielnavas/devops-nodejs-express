2eb1c5e94212ebb448e74a200114ea706369d858
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-01-30 16:49:43 -0300

    basic express server

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    hello: "world"
+  })
+})
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
