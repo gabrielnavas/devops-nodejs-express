498fc92a67bb0bdac6371d213c3d37a7f0e32684
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-01-31 20:24:03 -0300

    update docker ignore

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 # para iniciar, use sh init.sh <nome da funcao>
 
 dev_init () {
-  docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+  docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
 }
 
-dev_init () {
+dev_down () {
   docker-compose -f docker-compose.yml -f docker-compose.dev.yml down -v
 }
 
 production_init() {
-  docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d --build
 }
 
 production_down() {
